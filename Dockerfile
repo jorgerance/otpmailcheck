130eef0fb09ce449bed31825b45461aff4b8174e
Paul Miller <paul@paulmillr.com>
2021-11-07 14:51:12 +0100

    first commit

---
+++ Dockerfile
@@ -0,0 +1,39 @@
+###########################################
+# Throwaway image with C compiler installed
+
+FROM ubuntu:20.04
+
+# Install g++ for compilig uWSGI
+
+COPY ./requirements.txt /tmp/
+
+ENV PYTHONUNBUFFERED 1
+#RUN python -m pip install pip==9.0.3
+#RUN pip install cython
+#RUN pip install --install-option="--prefix=/install" --trusted-host pypi.python.org -r /tmp/requirements.txt
+RUN apt-get update
+RUN apt-get install -y build-essential python3 python3-dev python3-pip locales
+
+RUN locale-gen en_US.UTF-8
+ENV LANG en_US.UTF-8
+ENV LANGUAGE en_US:en
+ENV LC_ALL en_US.UTF-8
+
+RUN pip3 install --trusted-host pypi.python.org cython
+RUN pip3 install --trusted-host pypi.python.org --upgrade pip setuptools wheel
+#RUN pip3 install --trusted-host pypi.python.org pandas
+RUN pip3 install --trusted-host pypi.python.org -r /tmp/requirements.txt
+
+###########################################
+# Image WITHOUT C compiler but WITH uWSGI
+
+#FROM base
+#COPY --from=builder /install/ /usr/local
+#COPY --from=builder /root/.local/ /usr/.local
+
+# RUN apk add --no-cache libxml2 libxslt-dev jpeg-dev postgresql-dev
+
+USER 1000
+WORKDIR /app
+
+CMD [ "python3", "check_email.py" ]
---
+++ check_email.py
@@ -0,0 +1,170 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+
+import imaplib
+import email
+from email.header import decode_header
+import os
+import base64
+import html2text
+import re
+import requests
+import json
+import pandas as pd
+
+
+# account credentials
+username = os.getenv('OTP_EMAIL_USER')
+password = os.getenv('OTP_EMAIL_PASS')
+imap_server = os.getenv('OTP_IMAP_SERVER')
+pushover_app_token = os.getenv('PUSHOVER_APP_TOKEN')
+pushover_user_token = os.getenv('PUSHOVER_USER_TOKEN')
+
+
+email_archive = "emails.json"
+
+
+def fetch_previous_emails(file=email_archive):
+    id_list = []
+    try:
+        df = pd.read_json(file)
+        for email_id in df['id']:
+            if email_id not in id_list:
+                id_list.append(email_id)
+    except:
+        pass
+
+    return(id_list)
+
+
+def save_email(data):
+    a = []
+    if not os.path.isfile(email_archive):
+        a.append(data)
+        with open(email_archive, mode='w') as f:
+            f.write(json.dumps(a, indent=4))
+    else:
+        with open(email_archive) as feedsjson:
+            feeds = json.load(feedsjson)
+
+        feeds.append(data)
+        with open(email_archive, mode='w') as f:
+            f.write(json.dumps(feeds, indent=4))
+
+def clean(text):
+    # clean text for creating a folder
+    return "".join(c if c.isalnum() else "_" for c in text)
+
+
+
+def send_pushover(message='testing', title='Binance Email OTP', priority='1', sound='intermission'):
+    app_token = pushover_app_token
+    user_token = pushover_user_token
+    r = requests.post("https://api.pushover.net/1/messages.json", data={"token":app_token,"user":user_token,"message":message, "title": title, "priority": priority, "sound": sound})
+
+    return r.status_code
+
+
+
+# create an IMAP4 class with SSL
+imap = imaplib.IMAP4_SSL(imap_server)
+# authenticate
+imap.login(username, password)
+
+
+status, messages = imap.select("INBOX")
+# number of top emails to fetch
+N = 3
+# total number of emails
+messages = int(messages[0])
+
+
+for i in range(messages, messages-N, -1):
+    # fetch the email message by ID
+    res, msg = imap.fetch(str(i), "(RFC822)")
+    for response in msg:
+        if isinstance(response, tuple):
+            # parse a bytes email into a message object
+            msg = email.message_from_bytes(response[1])
+
+            # decode the email subject
+            subject, encoding = decode_header(msg["Subject"])[0]
+            if isinstance(subject, bytes):
+                # if it's a bytes, decode to str
+                subject = subject.decode(encoding)
+
+            # decode email sender
+            From, encoding = decode_header(msg.get("From"))[0]
+            if isinstance(From, bytes):
+                From = From.decode(encoding)
+
+            # decode email sender
+            Date, encoding = decode_header(msg.get("Date"))[0]
+            if isinstance(Date, bytes):
+                Date = Date.decode(encoding)
+
+            Message_id, encoding = decode_header(msg.get("Message-ID"))[0]
+            if isinstance(Message_id, bytes):
+                Message_id = Message_id.decode(encoding)
+
+            rich_body = msg.get_payload()
+
+            try:
+              body = html2text.html2text(base64.urlsafe_b64decode(rich_body.replace('-_', '+/').encode('ASCII')).decode())
+            except:
+              pass
+
+            for line in body.splitlines():
+
+              otps = re.findall(r'^\d{6,6}\s', line)
+
+              if len(otps) == 1 and 'Your verification code' in body:
+
+                otp = otps[0]
+
+                data = {
+                  'subject': subject,
+                  'from': From,
+                  'date': Date,
+                  'id': Message_id,
+                  'otp': otp
+                }
+
+                previous_emails_list = fetch_previous_emails()
+
+                if Message_id not in previous_emails_list:
+                  for k,v in data.items():
+                    print(k,v)
+                  save_email(data)
+                  send_pushover(message=otp)
+
+                break
+
+              #print(msg.as_string())
+
+              # get the email body
+              '''try:
+                rich_body = msg.get_payload()
+                body = html2text.html2text(base64.urlsafe_b64decode(rich_body.replace('-_', '+/').encode('ASCII')).decode())
+              except:
+                break'''
+
+
+
+                #for line in body.splitlines():
+
+              '''print('-'*60)
+              print('Subject:', subject)
+              print('Sender:', From)
+              print('Date:', Date)
+              print('otp:', otp)
+
+              save_email(data)'''
+
+
+
+
+# close the connection and logout
+imap.close()
+imap.logout()
